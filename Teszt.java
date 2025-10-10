e143fb8ecfbf27cc0c115752ef3ab5310ec8c90a
Aniko0619 <fulopne.kiss.aniko@gmail.com>
2025-10-10 18:57:57 +0200

    Megint egy új

--- Teszt.java
+++ Teszt.java
@@ -2,6 +2,6 @@
 
 public class Teszt {
     public static void main(String[] args) {
-        System.out.println("HELLO WORD Ujcsi");
+        System.out.println("Üdvözlök minden kedves Projecttársamat");
     }
 }
