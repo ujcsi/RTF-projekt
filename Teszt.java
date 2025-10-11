f7103a0e099e3d1afdfb3fa44ba80001d1df433a
janoscsatlos-prog <janos.csatlos@gmail.com>
2025-10-11 09:22:17 +0200

    Jani commit újra.

--- Teszt.java
+++ Teszt.java
@@ -3,6 +3,6 @@
 public class Teszt {
     public static void main(String[] args) {
         System.out.println("Üdvözlök minden kedves Projecttársamat");
-        System.out.println("Második sor");
+        System.out.println("Második sor3");
     }
 }
