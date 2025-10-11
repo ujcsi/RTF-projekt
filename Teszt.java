c2b077e2d6a3f6068b3f1a6b4b885dc74500dd31
Aniko0619 <fulopne.kiss.aniko@gmail.com>
2025-10-11 09:36:11 +0200

    Nyuszi új comment

--- Teszt.java
+++ Teszt.java
@@ -3,6 +3,6 @@
 public class Teszt {
     public static void main(String[] args) {
         System.out.println("Üdvözlök minden kedves Projecttársamat");
-        System.out.println("Második sor 1");
+        System.out.println("Második új sor Nyuszi");
     }
 }
