ec3254ab704aaa47b5950488f87dc0e38a4c42b4
Aniko0619 <fulopne.kiss.aniko@gmail.com>
2025-12-06 16:21:27 +0100

    Projekt nyilv.tart.folyt2

--- Teszt.java
+++ Teszt.java
@@ -6,6 +6,9 @@ public class Teszt {
         System.out.println("Második sor3");
         System.out.println("Második sor Ujcsi");
         System.out.println("Második új sor Nyuszi");
-        System.out.println("Hatodik sor");
+        System.out.println("Ujcsi uj branch");
+        System.out.println("Ötödik sor");
+        System.out.println("Ötödik sor");
+        System.out.println("Hatodik sor javítvafdfdf");
     }
 }
